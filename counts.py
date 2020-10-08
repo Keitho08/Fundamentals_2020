221a8f52b76654fd20f119aa554c8c2ebf205aa5
Keitho08 <59957178+Keitho08@users.noreply.github.com>
2020-10-08 18:34:54 +0100

    Repository and j-notebook created

---
+++ counts.py
@@ -0,0 +1,10 @@
+# Keith Brazill
+# G00387845
+
+# Write a Python function called counts that takes a list as
+# input and returns a dictionary of unique items in the list as keys and the number of
+# times each item appears as values. So, the input ['A', 'A', 'B', 'C', 'A']
+# should have output {'A': 3, 'B': 1, 'C': 1} . Your code should not depend
+# on any module from the standard library or otherwise. You should research the task
+# first and include a description with references of your algorithm in the notebook.
+
