0f2fd75606d037b1091983c285df585dc63201d7
Keitho08 <59957178+Keitho08@users.noreply.github.com>
2020-10-11 16:45:57 +0100

    Task 1 first draft complete. References, narrative and general clean up to be done

--- counts.py
+++ counts.py
@@ -8,3 +8,17 @@
 # on any module from the standard library or otherwise. You should research the task
 # first and include a description with references of your algorithm in the notebook.
 
+# First we are going to create a list
+lst = [] 
+  
+# We then prompt the user to tell us how many elements are in this list
+n = int(input("Enter number of items in list: ")) 
+  
+# We then tell python to let the user input the items in the list from 0 to total number input (n)
+for i in range(0, n): 
+    item = (input()) 
+  
+    lst.append(item) # adding the items to the list
+
+dict((x, lst.count(x)) for x in lst) # Here we tell python using the dict comamand that for each item in the list (x)
+                                     # we want a count of the number of times the item (x) appears in the list
